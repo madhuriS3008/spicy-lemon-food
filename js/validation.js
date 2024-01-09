0778a769ad3696716105a66200e94d9d73511a57
Madhuri Sabbineni <madhusabbineni@gmail.com>
2024-01-09 16:49:36 -0600

    made enhancements to sign up form and more validations on enhancements

--- js/validation.js
+++ js/validation.js
@@ -2,9 +2,29 @@ let password = document.getElementById("password");
 let confirmPassword = document.getElementById("confirm_password");
 let errorMsg = document.getElementById("error_msg");
 
+let birthDate = document.getElementById("birthday");
+let birthMonth = document.getElementById("birth_month");
+
 function validate() {
-  if (password.value == confirmPassword.value) return;
+  if (password.value == confirmPassword.value) errorMsg.innerText = "";
   else errorMsg.innerText = "Paswords does not match";
+}
+
+function birthDateValues() {
+  let mon = birthMonth.value;
+  if (
+    mon == "Jan" ||
+    mon == "Mar" ||
+    mon == "May" ||
+    mon == "Jul" ||
+    mon == "Aug" ||
+    mon == "Oct" ||
+    mon == "Dec"
+  ) {
+    birthDate.setAttribute("max", "31");
+  } else if (mon == "Feb") {
+    birthDate.setAttribute("max", "29");
+  } else birthDate.setAttribute("max", "30");
 }
 
 let showPassword = document.getElementById("show");
@@ -17,4 +37,7 @@ function showWord() {
 }
 showPassword.addEventListener("click", showWord);
 
+birthMonth.addEventListener("change", birthDateValues);
+
 confirmPassword.addEventListener("blur", validate);
+password.addEventListener("blur", validate);
